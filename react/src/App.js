0547a5afabc9911122fd9872007666048173fa54
Brodie Balser <londel@Brodies-MacBook-Pro.local>
2018-06-02 11:38:20 -0600

    added routes for regInfo and its comps

--- react/src/App.js
+++ react/src/App.js
@@ -5,7 +5,10 @@ import createHistory from 'history/createBrowserHistory'
 import logo from './logo.svg';
 import './App.css';
 import Home from './comp/Home'
-import Services from './comp/Services'
+import singleReg from './comp/regInfo/singleReg'
+import teamReg from './comp/regInfo/teamReg'
+import infoLandingPage from './comp/regInfo/infoLandingPage'
+
 
 const history = createHistory()
 
@@ -39,8 +42,15 @@ class App extends Component {
 						<Route exact path="/" render= {() => <Home
 							history={history}/>}/>
 
-						<Route path="/Services" render= {() => <Services
+						<Route path="/infoLandingPage" render= {() => <infoLandingPage
 							history={history}/>} />
+
+						<Route path="/singleReg" render= {() => <singleReg
+							history={history}/>} />
+
+						<Route path="/teamReg" render= {() => <teamReg
+							history={history}/>} />
+
 
 						<Route path="*" component={ErrorPage} />
 					</Switch>
