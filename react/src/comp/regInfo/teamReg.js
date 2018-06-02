ca6dcab1775e963316bdf34ffa31db48657628cd
chris <chrisrogers1017@gmail.com>
2018-06-02 13:44:18 -0600

    changes

--- react/src/comp/regInfo/teamReg.js
+++ react/src/comp/regInfo/teamReg.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react'; 
+// import React, { Component } from 'react'; 
 
-class TeamReg extends Component {
-	constructor(props) {
-		super(props); 
+// class TeamReg extends Component {
+// 	constructor(props) {
+// 		super(props); 
 
-	}
+// 	}
 
-	render(){
-		return (
+// 	render(){
+// 		return (
 
-			)
-	}
-}
+// 			)
+// 	}
+// }
