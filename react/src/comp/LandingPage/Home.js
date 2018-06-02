8e5df36fbc80bec2f8590cfbe19a26fd1b805d63
chris <chrisrogers1017@gmail.com>
2018-06-02 12:28:58 -0600

    landing page

--- react/src/comp/LandingPage/Home.js
+++ react/src/comp/LandingPage/Home.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import logo from "../../images/logo.png"
+import "./Home.css"
 
 const Home = () => {
 
 	return (
-		<div>
-			<button>Log-in</button>
-			<button>Sign-up</button>
-			<img src={logo} alt="Bootcampers Collective" />
+
+		
+		<div class="container">
+			<div class ="row" id="row1">
+				<div class="col-sm" id="header">
+					<header>
+						<h2><a href="#">Bootcampers Collective</a></h2>
+						<nav>
+							<li><a href="#">Log-in</a></li>
+							<li><a href="#">Sign-up</a></li>
+							<li><a href="#">Contact</a></li>
+						</nav>
+					</header>
+				</div>
+			</div>
+			<div class="row" id="row2">
+				<div class="col-sm" id="landingbody">
+					<section class="hero">
+						<div class="background-image"></div>
+						<div class="hero-content-area">
+						<img src={logo} alt="Bootcampers Collective" />
+							<h3>Hack-A-Thon</h3>
+							{/* <a href="#" class="btn">Contact Us Now</a> */}
+						</div>
+					</section>
+				</div>
+			</div>
 
 		</div>
 	)
