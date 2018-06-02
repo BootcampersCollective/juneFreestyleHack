b2b072a26a32dbf2bc287689f35719a416156276
Kenneth Brisco <kennethbrisco1@gmail.com>
2018-06-02 11:50:40 -0600

    Dong.

--- server/controllers/routes.js
+++ server/controllers/routes.js
@@ -1,13 +1,14 @@
 // TODO: Password reset, password change, login error handling and routing, sign up error handling.
-var objectId        = require('mongodb').ObjectId,
-    bcrypt          = require('bcrypt-nodejs'),
-    nodemailer      = require('nodemailer'),
-    // xoath2          = require('xoauth2'),
-    randomString    = require('randomstring'),
-    aws             = require('aws-sdk'),
-    fileUpload      = require('express-fileupload');
+var objectId = require("mongodb").ObjectId,
+  bcrypt = require("bcrypt-nodejs"),
+  nodemailer = require("nodemailer"),
+  // xoath2          = require('xoauth2'),
+  randomString = require("randomstring"),
+  aws = require("aws-sdk"),
+  fileUpload = require("express-fileupload");
 
-module.exports = function(app, passport) {
-
-
-    };
+module.exports = (app, passport) => {
+  app.get("/", (req, res) => {
+    res.send("Hello World");
+  });
+};
