3810a3a4a0823e279e7c7c31a8897b336f6ee2c1
spjohnson <spgant@gmail.com>
2018-06-02 11:46:34 -0600

    committing components all capitalized

---
+++ react/src/comp/regInfo/infoLandingPage.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'; 
+
+class InfoLandingPage extends Component {
+	constructor(props) {
+		super(props); 
+		this.state = {
+
+		}
+
+
+	}
+
+	render() {
+		return (
+			<div id="title">
+				<h1>Bootcampers Collective</h1>
+					<button id="singleReg">Single Registration</button> 
+					<button id="teamReg">Team Registration</button> 
+			</div> 
+
+
+			)
+	}
+}
+
+export default InfoLandingPage; 
